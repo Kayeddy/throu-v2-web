55d4f4facd5124b457c8aea51c1531fa493982a0
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2024-11-28 00:26:48 -0500

    refactor(project): Update project images for better performance

--- src/components/marketplace/IndividualProjectDetails.tsx
+++ src/components/marketplace/IndividualProjectDetails.tsx
@@ -30,22 +30,22 @@ const CardSkeletonLoader = () => {
 
 export const projectMedia = [
   // Render images
-  "/assets/projects/prado/renders/render_1.jpg",
-  "/assets/projects/prado/renders/render_2.jpg",
-  "/assets/projects/prado/renders/render_3.jpg",
-  "/assets/projects/prado/renders/render_4.jpg",
-  "/assets/projects/prado/renders/render_5.png",
-  "/assets/projects/prado/renders/render_6.png",
-  "/assets/projects/prado/renders/render_7.png",
-  "/assets/projects/prado/renders/render_8.png",
-  "/assets/projects/prado/renders/render_9.png",
+  "/assets/projects/prado/renders/render_1.webp",
+  "/assets/projects/prado/renders/render_2.webp",
+  "/assets/projects/prado/renders/render_3.webp",
+  "/assets/projects/prado/renders/render_4.webp",
+  "/assets/projects/prado/renders/render_5.webp",
+  "/assets/projects/prado/renders/render_6.webp",
+  "/assets/projects/prado/renders/render_7.webp",
+  "/assets/projects/prado/renders/render_8.webp",
+  "/assets/projects/prado/renders/render_9.webp",
 
   // Sketch images
-  "/assets/projects/prado/sketches/sketch_1.png",
-  "/assets/projects/prado/sketches/sketch_2.png",
-  "/assets/projects/prado/sketches/sketch_3.png",
-  "/assets/projects/prado/sketches/sketch_4.png",
-  "/assets/projects/prado/sketches/sketch_5.png",
+  "/assets/projects/prado/sketches/sketch_1.webp",
+  "/assets/projects/prado/sketches/sketch_2.webp",
+  "/assets/projects/prado/sketches/sketch_3.webp",
+  "/assets/projects/prado/sketches/sketch_4.webp",
+  "/assets/projects/prado/sketches/sketch_5.webp",
 ];
 
 export default function IndividualProjectDetails({
